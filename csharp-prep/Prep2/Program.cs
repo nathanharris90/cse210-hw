640465f4ec4ba227646b3a7afc7c51f5260ab707
nathan90@byui.edu <nathan90@byui.edu>
2023-11-01 23:12:27 -0600

    Prep 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your GPA? ");
+        float gpa = float.Parse(Console.ReadLine());
+
+        string grade = "";
+
+        if (gpa >= 90)
+        {
+            grade = "A";
+        }
+        else if (gpa >= 80)
+        {
+            grade = "B";
+        }
+        else if (gpa >= 70)
+        {
+            grade = "C";
+        }
+        else if (gpa >= 60)
+        {
+            grade = "D";
+        }
+        else if (gpa < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a {grade}.");
+
+        if (gpa >= 70)
+        {
+            Console.WriteLine("Congradulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class.");
+        }
     }
 }
