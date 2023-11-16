1ec90e606c0679eb1907ae3f333d3869d5e4466f
nathan90@byui.edu <nathan90@byui.edu>
2023-11-15 18:53:28 -0700

    Prep 03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,34 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_numerator}/{_denominator}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / (double)_denominator;
+    }
+}
