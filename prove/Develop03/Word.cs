2cd2892b13ee750b2380b896d3c49f7b1767f22b
nathan90@byui.edu <nathan90@byui.edu>
2023-11-19 17:54:49 -0700

    W3 Prove Scripture Memorizer

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,21 @@
+public class Word
+{
+    public string HideWord(string currentDisplay)
+    {
+        string[] _spliced = currentDisplay.Split(' ');
+        Random random = new Random();
+
+        int remainingWords = _spliced.Length;
+        if (remainingWords > 0)
+        {
+            int randomIndex = random.Next(0, _spliced.Length);
+            _spliced[randomIndex] = new string('_', _spliced[randomIndex].Length);
+            string updatedDisplay = string.Join(" ", _spliced);
+            return updatedDisplay;
+        }
+        else
+        {
+            return "All the words are gone.";
+        }
+    }
+}
