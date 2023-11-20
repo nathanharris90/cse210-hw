e6940734033143b08b9c545ea961e6ba58d8c84a
nathan90@byui.edu <nathan90@byui.edu>
2023-11-19 18:16:01 -0700

    Updated prove W3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,43 +2,7 @@ using System;
 using System.Data.Common;
 using System.Security.Cryptography;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Reference r1 = new Reference("Alma", 37, 36);
-//         Scripture s1 = new Scripture();
-//         Scripture u1 = new Scripture();
-//         string currentDisplay = s1.GetFullScripture();
 
-//         Console.Clear();
-
-//         Console.WriteLine(r1.GetReference() + currentDisplay);
-
-//         Console.WriteLine("Press the enter key to hide words, or press q and then enter to quit.");
-//         string _userInput = Console.ReadLine();
-
-//         while (_userInput != "q")
-//         {
-//             if (_userInput == "")
-//             {
-//                 currentDisplay = u1.HideWord(currentDisplay);
-//                 Console.Clear();
-//                 Console.WriteLine(r1.GetReference() + currentDisplay);
-//                 _userInput = Console.ReadLine();
-
-
-
-//             }
-//             else
-//             {
-//                 Console.WriteLine($"Please enter valid argument. Press enter to hide words, or q to quit.");
-//                 _userInput = Console.ReadLine();
-//             }
-//         }
-
-//     }
-// }
 class Program
 {
     static void Main(string[] args)
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -6,7 +6,7 @@ public class Word
         Random random = new Random();
 
         int remainingWords = _spliced.Length;
-        if (remainingWords > 0)
+        if (remainingWords > 25)
         {
             int randomIndex = random.Next(0, _spliced.Length);
             _spliced[randomIndex] = new string('_', _spliced[randomIndex].Length);
@@ -15,7 +15,8 @@ public class Word
         }
         else
         {
-            return "All the words are gone.";
+            string end = "All the words are gone";
+            return end;
         }
     }
 }
