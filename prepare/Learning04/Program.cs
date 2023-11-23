64e3407d03524f461444101314b3e0b6b6601390
nathan90@byui.edu <nathan90@byui.edu>
2023-11-23 16:15:58 -0700

    Inital Prepare W04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Nathan Harris", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment m1 = new MathAssignment("Ricky Bobby", "Fractions", "1.2", "1-2");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetMathInformation());
+
+        WritingAssignment w1 = new WritingAssignment("Beth Hilton", "American History", "History at its Finest");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
